958c6b06b9dd4c1070e2c0a1c5a2823e75a9e0ef
PredaaA <46051820+PredaaA@users.noreply.github.com>
2022-02-20 03:21:54 +0100

    Use Ackee programtically instead of script tag.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
-import App from './App.vue'
-import './index.css'
+import * as ackeeTracker from 'ackee-tracker';
+import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
-import DiscordTimestamps from './views/DiscordTimestamps.vue'
-import Home from './views/Home.vue'
-import NotFound from './views/NotFound.vue'
-import Privacy from './views/Privacy.vue'
+import App from './App.vue';
+import './index.css';
+import DiscordTimestamps from './views/DiscordTimestamps.vue';
+import Home from './views/Home.vue';
+import NotFound from './views/NotFound.vue';
+import Privacy from './views/Privacy.vue';
 
+
+ackeeTracker.create('https://ackee.predamart.me').record('5b9ea186-a300-4a42-803f-5bc0582b6ca6');
 
 const routes = [
   { path: '/', component: Home },
